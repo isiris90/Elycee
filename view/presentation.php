604a92f09f0a7881bd56ae5d860bc86d19cf3a3e
isiris90 <mel.leclere.22@gmail.com>
2015-08-06 11:04:08 +0200

    ajout texte presentation

---
+++ view/presentation.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/reset.css" type="text/css">
+    <link rel="stylesheet" href="../css/bootstrap.css" type="text/css">
+    <script src="../js/bootstrap.min.js"></script>
+
+    <title>Mentions légales</title>
+  </head>
+
+  <body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <!--ICI LA BARRE DE NAVIGATION -->
+                <?php include("header.php"); ?>
+
+                <div class="jumbotron">
+                    <h2>Présentation du lycée d'Ardèche</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row-actu">
+            <div class="col-md-12">
+                <div class="lesactus">
+                    <h3>Notre devise</h3>
+                    <p>Notre devise est simple, nous cherchons l'excellence. Notre lycée possède 99,9% d'optention au bac avec une pourcentage de mention supérieures à de nombreuses écoles. </p>
+                    <h3>Pourquoi notre lycée ?</h3>
+                    <p>Notre lycée est reconnu dans l'ensemble de sa région. Il est spécialisé dans les fillières scientifiques. Nous possèdons des installations spécialisées pour manipuler et faire des expèriences scientifiques pointues. En clair tout est là pour vous faire réussir votre cursus dans les meilleures conditions possibles. </p>
+                    <h3>Comment nous rejoindre</h3>
+                    <p>L'entrée dans notre établissement est exclusivement pour une population de personnes prète a faire des conscessions mais surtout avec un potentiel élevé. L'examen d'entrée est en plusieurs phases de test sur des sujets différents qui testerons vos capacités mais aussi votre mentale. Il est important d'être à donner le meilleur de soit même afin de réussir son cursus.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--ICI LE FOOTER -->
+    <?php include("footer.php"); ?>
+
+  </body>
+
+</html>
